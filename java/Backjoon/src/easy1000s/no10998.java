e6adf4e772cd4f10c7573181e26b57a2cf525fce
angietest <gydms0136@gmail.com>
2022-03-20 00:00:00 +0900

    Create no10998

---
+++ java/Backjoon/src/easy1000s/no10998.java
@@ -0,0 +1,17 @@
+package easy1000s;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class no10998 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] str = br.readLine().split(" ");
+		int A = Integer.parseInt(str[0]);
+		int B = Integer.parseInt(str[1]);
+		
+		System.out.println(A*B);
+		
+		}
+}
---
+++ java/Backjoon/src/easy1000s/no10998BufferedReader.java
@@ -0,0 +1,17 @@
+package easy1000s;
+
+import java.util.Scanner;
+
+public class no10998BufferedReader {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int A=sc.nextInt();
+		int B=sc.nextInt();
+		
+		System.out.println(A*B);
+			
+		sc.close();
+	}
+}
