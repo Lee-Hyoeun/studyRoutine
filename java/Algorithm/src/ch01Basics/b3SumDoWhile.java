f58ab080783dcee2510864d33c0edd5191f4e9a5
angietest <gydms0136@gmail.com>
2022-03-20 23:55:14 +0900

    Modify ch01 b

--- java/Algorithm/src/ch01Basics/b3SumDoWhile.java
+++ java/Algorithm/src/ch01Basics/b3SumDoWhile.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class b3SumDoWhile {
 	public static void main(String[] args) {
 		
-		System.out.println("정수를 입력하세요:");
+		System.out.println("정수를 입력하세요");
 		
 		Scanner sc = new Scanner(System.in);
 		int num;
--- java/Algorithm/src/ch01Basics/bEx2.java
+++ java/Algorithm/src/ch01Basics/bEx2.java
@@ -1,4 +1,6 @@
 package ch01Basics;
+// 두 변수 a, b에 정수를 입력하고 b-a를 출력하는 프로그램 작성
+// 양의 정수를 입력하고 자릿수를 출력하는 프로그램을 작성
 
 public class bEx2 {
 
