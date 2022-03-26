eb258b9ecae293a9b3c5c50b8d3f1f19952fc5d6
angietest <gydms0136@gmail.com>
2022-03-26 20:02:11 +0900

    Update a8CardConvRev.java

--- java/Algorithm/src/ch02Array/a8CardConvRev.java
+++ java/Algorithm/src/ch02Array/a8CardConvRev.java
@@ -1,4 +1,7 @@
 package ch02Array;
+
+import java.util.Scanner;
+
 // 입력 받은 10진수를 2진수~36진수로 기수 변환하여 나타냄
 
 public class a8CardConvRev {
@@ -12,6 +15,39 @@ public class a8CardConvRev {
 			x /= r;
 		} while(x != 0);
 		return digits;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int no; //변환하는 정수
+		int cd; //기수
+		int dno; //변환후의 자릿수
+		int retry; //재시도
+		char[] cno = new char[32];
+		
+		System.out.println("10진수를 기수 변환");
+		do {
+			do {
+				System.out.print("변환하는 음이 아닌 정수: ");
+				no = sc.nextInt();
+			} while(no<0);
+			
+			do {
+				System.out.print("어떤 진수로 변환? (2~36): ");
+				cd = sc.nextInt();
+			} while(cd<2 || cd>36);
+			dno = cardConvR(no, cd, cno); //no를 cd진수로 변환
+			
+			System.out.print(cd+"진수로는 ");
+			for(int i=dno-1; i>=0; i--) {
+				System.out.print("cno[i]");
+			}
+			System.out.println("입니다.");
+			
+			System.out.print("한번 더? (1.예 / 2.아니오): ");
+			retry = sc.nextInt();
+		} while(retry == 1);
+		
 	}
 
 }
