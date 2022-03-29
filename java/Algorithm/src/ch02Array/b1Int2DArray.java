e65c4fbf4014441c624f046bc433cec9b329712b
angietest <gydms0136@gmail.com>
2022-03-29 22:45:19 +0900

    Create b1Int2DArray.java

---
+++ java/Algorithm/src/ch02Array/b1Int2DArray.java
@@ -0,0 +1,20 @@
+package ch02Array;
+// 2행 4열의 2차원 배열
+
+public class b1Int2DArray {
+	public static void main(String[] args) {
+		int[][] x = new int[2][4]; //2차원 배열을 선언
+		
+		x[0][1] = 37;
+		x[0][3] = 54;
+		x[1][2] = 65;
+		
+		for(int i=0; i<2; i++) {
+			for(int j=0; j<4; j++) {
+				System.out.println("x["+i+"]["+j+"] = "+x[i][j]);
+			}
+		}
+		
+	}
+
+}
