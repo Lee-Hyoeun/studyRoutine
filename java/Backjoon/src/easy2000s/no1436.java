01afc13f7899cd80481d2969d1df7a8a57c4da10
angietest <gydms0136@gmail.com>
2022-04-12 23:31:26 +0900

    Create no1436.java

---
+++ java/Backjoon/src/easy2000s/no1436.java
@@ -0,0 +1,45 @@
+package easy2000s;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+
+//public class no1436 {
+//	public static void main(String[] args) {
+//		
+//		Scanner sc = new Scanner(System.in);
+//		int N = sc.nextInt();
+// 
+//		int num = 666;
+//		int count = 1;
+//        
+//		while(count != N) {
+//			num++;
+//			if(String.valueOf(num).contains("666")) {
+//				count++;
+//			}
+//		}
+//		System.out.println(num);
+//	}
+//}
+
+public class no1436 {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+ 
+		int num = 666;
+		int count = 1;
+        
+		while(count != N) {
+			num++;
+			if(String.valueOf(num).contains("666")) {
+				count++;
+			}
+		}
+		System.out.println(num);
+	}
+}
