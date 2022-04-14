76c01a2adf71dd7923e13f1da2f8870c9442abb1
angietest <gydms0136@gmail.com>
2022-04-14 16:27:54 +0900

    Update no1712.java

--- java/Backjoon/src/easy1000s/no1712.java
+++ java/Backjoon/src/easy1000s/no1712.java
@@ -1,16 +1,37 @@
 package easy1000s;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class no1712 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+// 
+// 
+//		int A = sc.nextInt();	// 불변 비용
+//		int B = sc.nextInt();	// 가변 비용
+//		int C = sc.nextInt(); 	// 상품 가격
+//        
+//		if (C <= B) {
+//			System.out.println("-1");
+//		} 
+//		else {
+//			System.out.println((A/(C-B))+1);
+//		}
+//	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
  
+		StringTokenizer st = new StringTokenizer(br.readLine());
  
-		int A = sc.nextInt();	// 불변 비용
-		int B = sc.nextInt();	// 가변 비용
-		int C = sc.nextInt(); 	// 상품 가격
-        
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+ 
 		if (C <= B) {
 			System.out.println("-1");
 		} 
@@ -18,4 +39,6 @@ public class no1712 {
 			System.out.println((A/(C-B))+1);
 		}
 	}
+	
+	
 }
