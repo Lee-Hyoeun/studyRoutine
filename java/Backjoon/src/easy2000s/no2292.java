8e8eee50b4284b9e41b4280582e6b713ba7ed657
angietest <gydms0136@gmail.com>
2022-04-22 22:56:14 +0900

    Update no2292.java

--- java/Backjoon/src/easy2000s/no2292.java
+++ java/Backjoon/src/easy2000s/no2292.java
@@ -1,13 +1,40 @@
 package easy2000s;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
+//public class no2292 {
+//	public static void main(String[] args) {
+//		 
+//		Scanner in = new Scanner(System.in);
+// 
+//		int N = in.nextInt();
+//		int count = 1; // 겹 수(최소 루트)
+//		int range = 2;	// 범위 (최솟값 기준) 
+// 
+//		if (N == 1) {
+//			System.out.print(1);
+//		}
+// 
+//		else {
+//			while (range <= N) {	// 범위가 N보다 커지기 직전까지 반복 
+//				range = range + (6 * count);	// 다음 범위의 최솟값으로 초기화 
+//				count++;	// count 1 증가 
+//			}
+//			System.out.print(count);
+//		}
+//	}
+//}
+
+
 public class no2292 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		 
-		Scanner in = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
  
-		int N = in.nextInt();
+		int N = Integer.parseInt(br.readLine());
 		int count = 1; // 겹 수(최소 루트)
 		int range = 2;	// 범위 (최솟값 기준) 
  
@@ -17,8 +44,8 @@ public class no2292 {
  
 		else {
 			while (range <= N) {	// 범위가 N보다 커지기 직전까지 반복 
-				range = range + (6 * count);	// 다음 범위의 최솟값으로 초기화 
-				count++;	// count 1 증가 
+				range = range + (6 * count);
+				count++;
 			}
 			System.out.print(count);
 		}
