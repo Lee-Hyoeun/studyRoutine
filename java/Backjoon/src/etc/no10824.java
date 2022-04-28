752b6fedec70c1686596bb84ac648d05fc1cffc7
Lee-Hyoeun <gydms0136@gmail.com>
2022-04-28 22:48:48 +0900

    Create no10824.java

---
+++ java/Backjoon/src/etc/no10824.java
@@ -0,0 +1,25 @@
+package etc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class no10824 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringBuilder sb = new StringBuilder();
+        String[] input = br.readLine().split(" ");
+        
+
+        long num1 = Long.parseLong(input[0]+input[1]);
+        long num2 = Long.parseLong(input[2]+input[3]);
+       
+        sb.append(num1 + num2);
+        
+        System.out.println(sb);
+
+    }
+
+}
