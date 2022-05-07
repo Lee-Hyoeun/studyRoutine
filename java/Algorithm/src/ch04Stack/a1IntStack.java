4900e6f3b2f030b5b636d049ccc33b338d7b572d
angietest <gydms0136@gmail.com>
2022-05-08 08:46:21 +0900

    Create a1IntStack.java

---
+++ java/Algorithm/src/ch04Stack/a1IntStack.java
@@ -0,0 +1,32 @@
+package ch04Stack;
+// int형 스택
+
+public class a1IntStack {
+	private int max; // 스택 용량
+	private int ptr; // 스택 포인터
+	private int[] stk; // 스택 본체
+	
+	// 실행시 예외 : 스택이 비어있음
+	public class EmptyIntStackException extends RuntimeException{
+		public EmptyIntStackException() {}	
+	}
+	
+	// 실행시 예외 : 스택이 가득 참
+	public class OverflowIntStackException extends RuntimeException{
+		public OverflowIntStackException() {}
+	}
+	
+	// 생성자
+	public a1IntStack(int capacity) {
+		ptr = 0;
+		max = capacity;
+		try {
+			stk = new int[max]; // 스택 본체용 배열을 생성
+		} catch (OutOfMemoryError e) { //생성할 수 없음
+			max = 0;
+			
+		}
+	}
+	
+}
+	
