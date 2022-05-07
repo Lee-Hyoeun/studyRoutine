5ae4e20f5998536a46a33b375c244bfc1bb6a2a3
angietest <gydms0136@gmail.com>
2022-05-07 21:19:41 +0900

    Update no2292BR.java

--- java/Backjoon/src/easy2000s/no2292BR.java
+++ java/Backjoon/src/easy2000s/no2292BR.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class no2292 {
+public class no2292BR {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
