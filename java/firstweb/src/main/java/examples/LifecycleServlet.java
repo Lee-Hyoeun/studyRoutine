46aea9cb0bc35c8b80c4f075fca95251b11d9348
angietest <gydms0136@gmail.com>
2022-05-11 22:04:35 +0900

    Update LifecycleServlet.java

--- java/firstweb/src/main/java/examples/LifecycleServlet.java
+++ java/firstweb/src/main/java/examples/LifecycleServlet.java
@@ -1,6 +1,8 @@
 package examples;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -36,11 +38,38 @@ public class LifecycleServlet extends HttpServlet {
         System.out.println("destroy 호출!");
 	}
 
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<head><title>form</title></head>");
+		out.println("<body>");
+		out.println("<form method='post' action='/firstweb/LifecycleServlet'>");
+		out.println("name : <input type='text' name='name'><br>");
+		out.println("<input type='submit' value='ok'><br>");                                                 
+		out.println("</form>");
+		out.println("</body>");
+		out.println("</html>");
+		out.close();
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		String name = req.getParameter("name");
+		out.println("<h1> hello " + name + "</h1>");
+		out.close();
+	}
+
 	/**
 	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("service 호출!");
-	}
+//	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        System.out.println("service 호출!");
+//	}
+	
+	
 
 }
