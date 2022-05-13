2b14153f305f6f2138215d6a46a845594a5c8ed0
angietest <gydms0136@gmail.com>
2022-05-13 12:18:00 +0900

    Update ParameterServlet.java

--- java/firstweb/src/main/java/examples/ParameterServlet.java
+++ java/firstweb/src/main/java/examples/ParameterServlet.java
@@ -1,6 +1,8 @@
 package examples;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,8 +28,21 @@ public class ParameterServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head><title>form</title></head>");
+		out.println("<body>");
+
+		String name = request.getParameter("name");
+		String age = request.getParameter("age");
+		
+		out.println("name : " + name + "<br>");
+		out.println("age : " +age + "<br>");
+		
+		out.println("</body>");
+		out.println("</html>");
+	}		
+	
 
 }
