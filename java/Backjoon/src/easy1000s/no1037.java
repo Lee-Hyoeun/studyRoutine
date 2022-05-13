d4435f6ae40707c37c268f4cb234ec09613c954e
angietest <gydms0136@gmail.com>
2022-05-13 15:50:11 +0900

    Create no1037.java

---
+++ java/Backjoon/src/easy1000s/no1037.java
@@ -0,0 +1,29 @@
+package easy1000s;
+
+import java.util.Scanner;
+
+public class no1037 {
+	public static void main(String[] args) {
+		 
+		Scanner in = new Scanner(System.in);
+		
+		int T = in.nextInt();
+		
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
+		
+		while(T-- > 0) {
+			int N = in.nextInt();
+			max = N > max ? N : max;
+			min = N < min ? N : min;
+            
+			/*
+			 same this
+			 if(N > max) max = N;
+			 if(N < min) min = N;
+			*/
+		}
+		System.out.println(max * min);
+	}
+
+}
