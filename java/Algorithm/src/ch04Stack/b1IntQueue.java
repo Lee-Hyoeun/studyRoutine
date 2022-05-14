2b7b42627ebf8bc4bebf7be58d20473b34a41c6d
angietest <gydms0136@gmail.com>
2022-05-15 08:36:07 +0900

    Update b1IntQueue.java

--- java/Algorithm/src/ch04Stack/b1IntQueue.java
+++ java/Algorithm/src/ch04Stack/b1IntQueue.java
@@ -30,6 +30,18 @@ public class b1IntQueue {
 		}
 	}
 	
-	
+	// 큐에 데이터를 인큐
+	public int enque(int x) throws OverflowIntQueueException{
+		if(num>=max) {
+			throw new OverflowIntQueueException();	//큐가 가득 참
+		}
+		
+		que[rear++] = x;
+		num++;
+		if(rear==max) {
+			rear = 0;
+		}
+		return x;
+	}
 
 }
