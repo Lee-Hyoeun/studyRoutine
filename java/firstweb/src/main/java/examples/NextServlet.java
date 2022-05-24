57d8535f5a60f2fac342851f753e20ea65b6c117
angietest <gydms0136@gmail.com>
2022-05-24 19:58:27 +0900

    Update NextServlet.java

--- java/firstweb/src/main/java/examples/NextServlet.java
+++ java/firstweb/src/main/java/examples/NextServlet.java
@@ -1,6 +1,8 @@
 package examples;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,7 +28,23 @@ public class NextServlet extends HttpServlet {
 	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head><title>form</title></head>");
+        out.println("<body>");
+
+        int dice = (Integer)request.getAttribute("dice"); //세탁소에서 맡긴거 찾아오기(setAttribute->getAttribute)
+        out.println("dice : " + dice);
+        
+        for(int i = 0; i < dice; i++) {
+            out.print("<br>hello");
+        }
+        
+        out.println("</body>");
+        out.println("</html>");
+	
 	}
 
 }
