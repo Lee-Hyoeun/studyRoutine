b7bec627b6ef69babee655b9ffebe4f93111afaf
angietest <gydms0136@gmail.com>
2022-05-30 17:37:17 +0900

    Update b1IntQueue.java

--- java/Algorithm/src/ch04Stack/b1IntQueue.java
+++ java/Algorithm/src/ch04Stack/b1IntQueue.java
@@ -18,8 +18,9 @@ public class b1IntQueue {
 		public OverflowIntQueueException() {}
 	}
 	
+
 	// 생성자
-	public void IntQueue(int capacity) {
+	public b1IntQueue(int capacity) {
 		num = front = rear = 0;
 		max = capacity;
 		
@@ -62,5 +63,64 @@ public class b1IntQueue {
 		
 	}
 	
+	
+	// 큐에서 데이터를 피크(프런트 데이터를 들여다봄)
+	public int peek() throws EmptyIntQueueException {
+		if(num <= 0) {
+			throw new EmptyIntQueueException();	// 큐가 비어있음
+		}
+		
+		return que[front];
+	}
+	
+	// 큐에서 x를 검색하여 인덱스(찾지 못하면 -1)를 반환
+	public int indexOf(int x) {
+		for (int i=0; i<num; i++) {
+			int idx = (i+front) % max;
+			
+			if(que[idx] == x) {	//검색 성공
+				return idx;
+			}
+			
+		}
+		
+		return -1;	//검색 실패
+	}
+	
+	// 큐를 비움
+	public void clear() {
+		num = front = rear = 0;
+	}
+	
+	// 큐의 용량을 반환
+	public int capacity() {
+		return max;
+	}
+	
+	// 큐에 쌓여 있는 데이터 수를 반환
+	public int size() {
+		return num;
+	}
+	
+	// 큐가 비어있으면?
+	public boolean isEmpty() {
+		return num >= max;
+	}
+	
+	// 큐 안의 모든 데이터를 프런트=>리어 순으로 출력
+	public void dump() {
+		if (num<=0) {
+			System.out.println("큐가 비어 있습니다.");
+		} else {
+			for(int i=0; i<num; i++) {
+				System.out.print(que[(i+front)%max]+ " ");
+			}
+		System.out.println();
+		
+		}
+	}
 
 }
+
+
+
