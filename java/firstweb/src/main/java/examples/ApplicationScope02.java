d2b6627a688659cd0ef78679b0ba7ffd6fd74814
angietest <gydms0136@gmail.com>
2022-06-01 14:54:03 +0900

    Create ApplicationScope02.java

---
+++ java/firstweb/src/main/java/examples/ApplicationScope02.java
@@ -0,0 +1,44 @@
+package examples;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ApplicationScope01
+ */
+@WebServlet("/ApplicationScope01")
+public class ApplicationScope02 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ApplicationScope02() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	
+		PrintWriter out = response.getWriter();
+		
+		ServletContext application = getServletContext(); //application scope에서 웹어플리케이션 영역=getServletContext객체=인터페이스를 구현하는 애
+		int value = 1;
+		application.setAttribute("value",value); //setAttribute에 맡김
+		
+		out.println("<h1>value : "+value+"/<h1>");
+		
+	}
+
+}
