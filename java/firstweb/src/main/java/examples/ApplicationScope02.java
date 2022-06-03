80921ebd4e9b644fc563cddecff093fefc57c640
angietest <gydms0136@gmail.com>
2022-06-03 14:52:05 +0900

    Update ApplicationScope02.java

--- java/firstweb/src/main/java/examples/ApplicationScope02.java
+++ java/firstweb/src/main/java/examples/ApplicationScope02.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ApplicationScope01
  */
-@WebServlet("/ApplicationScope01")
+@WebServlet("/ApplicationScope02")
 public class ApplicationScope02 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
