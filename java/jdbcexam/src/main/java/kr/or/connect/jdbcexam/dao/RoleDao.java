cb1504704aa65c1eb0bfe7c7fc09934d703e3a54
angietest <gydms0136@gmail.com>
2022-06-08 11:15:46 +0900

    Update Role mysql insert

--- java/jdbcexam/src/main/java/kr/or/connect/jdbcexam/dao/RoleDao.java
+++ java/jdbcexam/src/main/java/kr/or/connect/jdbcexam/dao/RoleDao.java
@@ -12,38 +12,30 @@ public class RoleDao {
 	private static String dburl = "jdbc:mysql://localhost:3306/connectdb";
 	private static String dbUser = "connectuser";
 	private static String dbpasswd = "connect123!@#";
-	
-	// role을 리턴하는 메소드
-	public Role getRole(Integer roleId) {
-		Role role = null;
+
+	// role을 insert하는 메소드
+	public int addRole(Role role) {
+		int insertCount = 0;
+		
 		Connection conn = null;
 		PreparedStatement ps = null;
-		ResultSet rs = null;
 		
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			conn = DriverManager.getConnection(dburl, dbUser, dbpasswd);
-			String sql = "SELECT description,role_id FROM role WHERE role_id = ?";
+			String sql = "INSERT INTO role (role_id, description) VALUES ( ?, ? )";
+			
 			ps = conn.prepareStatement(sql);
-			ps.setInt(1, roleId);
-			rs = ps.executeQuery();			
-		
-			if (rs.next()) {
-				String description = rs.getString(1);
-				int id = rs.getInt("role_id");
-				role = new Role(id, description);
-			}
+			
+			ps.setInt(1, role.getRoleId());
+			ps.setString(2, role.getDescription());
+
+			insertCount = ps.executeUpdate();
 			
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			if(rs != null) {
-				try {
-					rs.close();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
+			
 			if(ps != null) {
 				try {
 					ps.close();
@@ -62,7 +54,59 @@ public class RoleDao {
 		}
 		
 		
-		return role;
+		return insertCount;
 	}
+	
+//	// role을 리턴하는 메소드
+//	public Role getRole(Integer roleId) {
+//		Role role = null;
+//		Connection conn = null;
+//		PreparedStatement ps = null;
+//		ResultSet rs = null;
+//		
+//		try {
+//			Class.forName("com.mysql.cj.jdbc.Driver");
+//			conn = DriverManager.getConnection(dburl, dbUser, dbpasswd);
+//			String sql = "SELECT description,role_id FROM role WHERE role_id = ?";
+//			ps = conn.prepareStatement(sql);
+//			ps.setInt(1, roleId);
+//			rs = ps.executeQuery();			
+//		
+//			if (rs.next()) {
+//				String description = rs.getString(1);
+//				int id = rs.getInt("role_id");
+//				role = new Role(id, description);
+//			}
+//			
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		} finally {
+//			if(rs != null) {
+//				try {
+//					rs.close();
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//			}
+//			if(ps != null) {
+//				try {
+//					ps.close();
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//			}
+//			if(conn != null) {
+//				try {
+//					conn.close();
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//			}
+//			
+//		}
+//		
+//		
+//		return role;
+//	}
 
 }
