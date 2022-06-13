09eb54c8246613f5d8072e30d5c9a26d5d1f77e3
angietest <gydms0136@gmail.com>
2022-06-13 19:58:22 +0900

    Update RoleDao.java

--- java/jdbcexam/src/main/java/kr/or/connect/jdbcexam/dao/RoleDao.java
+++ java/jdbcexam/src/main/java/kr/or/connect/jdbcexam/dao/RoleDao.java
@@ -4,7 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-
+import java.util.ArrayList;
+import java.util.List;
 
 import kr.or.connect.jdbcexam.dto.Role;
 
@@ -12,7 +13,121 @@ public class RoleDao {
 	private static String dburl = "jdbc:mysql://localhost:3306/connectdb";
 	private static String dbUser = "connectuser";
 	private static String dbpasswd = "connect123!@#";
+	
+	// role을 select하는 메소드
+	public List<Role> getRoles() {
+		List<Role> list = new ArrayList<>();
 
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		String sql = "SELECT description, role_id FROM role order by role_id desc";
+		try (Connection conn = DriverManager.getConnection(dburl, dbUser, dbpasswd);
+				PreparedStatement ps = conn.prepareStatement(sql)) {
+
+			try (ResultSet rs = ps.executeQuery()) {
+
+				while (rs.next()) {
+					String description = rs.getString(1);
+					int id = rs.getInt("role_id");
+					Role role = new Role(id, description);
+					list.add(role); // list에 반복할때마다 Role인스턴스를 생성하여 list에 추가한다.
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return list;
+	}
+	
+	
+	// role을 update하는 메소드
+	public int updateRole(Role role) {
+		int updateCount = 0;
+		
+		
+		Connection conn = null;
+		PreparedStatement ps = null;
+		
+		try {
+			Class.forName( "com.mysql.jdbc.Driver" );
+			
+			conn = DriverManager.getConnection ( dburl, dbUser, dbpasswd );
+			
+			String sql = "update role set description = ? where role_id = ?";
+			
+			ps = conn.prepareStatement(sql);
+			
+			ps.setString(1, role.getDescription());
+			ps.setInt(2,  role.getRoleId());
+			
+			updateCount = ps.executeUpdate();
+
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}finally {
+			if(ps != null) {
+				try {
+					ps.close();
+				}catch(Exception ex) {}
+			} // if
+			
+			if(conn != null) {
+				try {
+					conn.close();
+				}catch(Exception ex) {}
+			} // if
+		} // finally
+		
+		return updateCount;
+	}
+	
+	
+	// role을 delete하는 메소드
+	public int deleteRole(Integer roleId) {
+		int deleteCount = 0;
+		
+		Connection conn = null;
+		PreparedStatement ps = null;
+		
+		try {
+			Class.forName( "com.mysql.jdbc.Driver" );
+			
+			conn = DriverManager.getConnection ( dburl, dbUser, dbpasswd );
+			
+			String sql = "DELETE FROM role WHERE role_id = ?";
+
+			ps = conn.prepareStatement(sql);
+			
+			ps.setInt(1,  roleId);
+
+			deleteCount = ps.executeUpdate();
+
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}finally {
+			if(ps != null) {
+				try {
+					ps.close();
+				}catch(Exception ex) {}
+			} // if
+			
+			if(conn != null) {
+				try {
+					conn.close();
+				}catch(Exception ex) {}
+			} // if
+		} // finally
+
+		return deleteCount;
+	}
+	
+	
 	// role을 insert하는 메소드
 	public int addRole(Role role) {
 		int insertCount = 0;
@@ -57,56 +172,58 @@ public class RoleDao {
 		return insertCount;
 	}
 	
-//	// role을 리턴하는 메소드
-//	public Role getRole(Integer roleId) {
-//		Role role = null;
-//		Connection conn = null;
-//		PreparedStatement ps = null;
-//		ResultSet rs = null;
-//		
-//		try {
-//			Class.forName("com.mysql.cj.jdbc.Driver");
-//			conn = DriverManager.getConnection(dburl, dbUser, dbpasswd);
-//			String sql = "SELECT description,role_id FROM role WHERE role_id = ?";
-//			ps = conn.prepareStatement(sql);
-//			ps.setInt(1, roleId);
-//			rs = ps.executeQuery();			
-//		
-//			if (rs.next()) {
-//				String description = rs.getString(1);
-//				int id = rs.getInt("role_id");
-//				role = new Role(id, description);
-//			}
-//			
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		} finally {
-//			if(rs != null) {
-//				try {
-//					rs.close();
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//				}
-//			}
-//			if(ps != null) {
-//				try {
-//					ps.close();
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//				}
-//			}
-//			if(conn != null) {
-//				try {
-//					conn.close();
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//				}
-//			}
-//			
-//		}
-//		
-//		
-//		return role;
-//	}
-
+	
+	// role을 리턴하는 메소드
+	public Role getRole(Integer roleId) {
+		Role role = null;
+		Connection conn = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection(dburl, dbUser, dbpasswd);
+			String sql = "SELECT description,role_id FROM role WHERE role_id = ?";
+			ps = conn.prepareStatement(sql);
+			ps.setInt(1, roleId);
+			rs = ps.executeQuery();			
+		
+			if (rs.next()) {
+				String description = rs.getString(1);
+				int id = rs.getInt("role_id");
+				role = new Role(id, description);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(rs != null) {
+				try {
+					rs.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			if(ps != null) {
+				try {
+					ps.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			if(conn != null) {
+				try {
+					conn.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			
+		}
+		
+		
+		return role;
+	}
 }
+
+
