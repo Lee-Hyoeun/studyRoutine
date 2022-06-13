d843614d54eb836e8f7e03f7a269cf5350ffcb2a
angietest <gydms0136@gmail.com>
2022-06-13 19:58:28 +0900

    Update JDBCExam1.java

--- java/jdbcexam/src/main/java/kr/or/connect/jdbcexam/JDBCExam1.java
+++ java/jdbcexam/src/main/java/kr/or/connect/jdbcexam/JDBCExam1.java
@@ -7,8 +7,8 @@ public class JDBCExam1 {
 
 	public static void main(String[] args) {
 		RoleDao dao = new RoleDao();
-		//Role role = dao.getRole(100);
-		//System.out.println(role);
+		Role role = dao.getRole(100);
+		System.out.println(role);
 	}
 
 }
