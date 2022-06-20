8391d9f22d35a69a83b7d56a1b8f1981ed9adf22
angietest <gydms0136@gmail.com>
2022-06-20 18:48:54 +0900

    Update ApplicationConfig.java
    study dto, dao

--- java/daoexam/src/main/java/kr/or/connect/daoexam/config/ApplicationConfig.java
+++ java/daoexam/src/main/java/kr/or/connect/daoexam/config/ApplicationConfig.java
@@ -1,10 +1,12 @@
 package kr.or.connect.daoexam.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 // import 여러 설정 지정해서 설정 가능(유지보수)
 
 @Configuration
+@ComponentScan(basePackages = { "kr.or.connect.daoexam.dao" })
 @Import(DBConfig.class)
 public class ApplicationConfig {
 
