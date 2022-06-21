33bfc711f4e2b0c2a1ff62b98076a5afe3c6a5c0
angietest <gydms0136@gmail.com>
2022-06-21 19:59:26 +0900

    Update RoleDaoSqls.java

--- java/daoexam/src/main/java/kr/or/connect/daoexam/dao/RoleDaoSqls.java
+++ java/daoexam/src/main/java/kr/or/connect/daoexam/dao/RoleDaoSqls.java
@@ -2,4 +2,8 @@ package kr.or.connect.daoexam.dao;
 
 public class RoleDaoSqls {
 	public static final String SELECT_ALL = "SELECT role_id, description FROM role order by role_id";
+
+	public static final String UPDATE = "UPDATE role SET description = :description WHERE ROLE_ID = :roleId";
+
+
 }
