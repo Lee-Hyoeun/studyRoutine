2bc3505c45160d361e119c0cd9efb98d6bfc8240
angietest <gydms0136@gmail.com>
2022-06-21 19:59:30 +0900

    Create JDBCTest.java

---
+++ java/daoexam/src/main/java/kr/or/connect/daoexam/main/JDBCTest.java
@@ -0,0 +1,29 @@
+package kr.or.connect.daoexam.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import kr.or.connect.daoexam.config.ApplicationConfig;
+import kr.or.connect.daoexam.dao.RoleDao;
+import kr.or.connect.daoexam.dto.Role;
+
+public class JDBCTest {
+	public static void main(String[] args) {
+		ApplicationContext ac = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+
+		RoleDao roleDao = ac.getBean(RoleDao.class);
+		
+		Role role = new Role();
+		role.setRoleId(500);
+		role.setDescription("CEO");
+//		role.setRoleId(201);
+//		role.setDescription("PROGRAMMER");
+		
+		int count = roleDao.insert(role);
+		System.out.println(count + "건 입력하였습니다.");
+			
+//		int count = roleDao.update(role);
+//		System.out.println(count +  " 건 수정하였습니다.");
+	}
+	
+}
