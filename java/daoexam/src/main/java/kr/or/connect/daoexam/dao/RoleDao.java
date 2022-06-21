fad3f54366a0ab6d54a1e36763fa19482e40117e
angietest <gydms0136@gmail.com>
2022-06-21 19:59:22 +0900

    Update RoleDao.java

--- java/daoexam/src/main/java/kr/or/connect/daoexam/dao/RoleDao.java
+++ java/daoexam/src/main/java/kr/or/connect/daoexam/dao/RoleDao.java
@@ -1,6 +1,6 @@
 package kr.or.connect.daoexam.dao;
 
-import static kr.or.connect.daoexam.dao.RoleDaoSqls.SELECT_ALL;
+import static kr.or.connect.daoexam.dao.RoleDaoSqls.*;
 
 import java.util.Collections;
 import java.util.List;
@@ -9,7 +9,9 @@ import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
@@ -31,6 +33,17 @@ public class RoleDao {
 	
 	public List<Role> selectAll(){
 		return jdbc.query(SELECT_ALL, Collections.<String, Object>emptyMap(), rowMapper);
+	}
+	
+	
+	public int insert(Role role) {
+		SqlParameterSource params = new BeanPropertySqlParameterSource(role);
+		return insertAction.execute(params);
+	}
+
+	public int update(Role role) {
+		SqlParameterSource params = new BeanPropertySqlParameterSource(role);
+		return jdbc.update(UPDATE, params);
 	}
 
 }
