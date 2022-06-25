44be0da1dc9a79ec50e60795c6d53b1ed223691b
angietest <gydms0136@gmail.com>
2022-06-25 23:22:21 +0900

    Create moreSpicy.java

---
+++ java/Backjoon/src/level2/moreSpicy.java
@@ -0,0 +1,29 @@
+package level2;
+
+import java.util.PriorityQueue;
+
+public class moreSpicy {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> heap = new PriorityQueue();
+
+        for (int aScoville : scoville) {
+            heap.offer(aScoville);
+        }
+
+        while (heap.peek() <= K) {
+            if (heap.size() == 1) {
+                return -1;
+            }
+            int a = heap.poll();
+            int b = heap.poll();
+
+
+            int result = a + (b * 2);
+
+            heap.offer(result);
+            answer ++;
+        }
+        return answer;
+    }
+}
