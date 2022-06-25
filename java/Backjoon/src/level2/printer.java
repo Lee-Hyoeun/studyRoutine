368e7586e135e151daf409a10e9854a9f85a5fba
angietest <gydms0136@gmail.com>
2022-06-25 23:31:57 +0900

    Create printer.java

---
+++ java/Backjoon/src/level2/printer.java
@@ -0,0 +1,29 @@
+package level2;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class printer {
+	public int solution(int[] priorities, int location) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        int answer = 0;
+        
+        for (int i = 0; i < priorities.length; i++) {
+            pq.add(priorities[i]);
+        }
+        
+        while (!pq.isEmpty()) {
+            for (int i = 0; i < priorities.length; i++) {
+                if (priorities[i] == pq.peek()) {
+                    if (i == location) {
+                        answer++;
+                        return answer;
+                    }
+                    pq.poll();
+                    answer++;
+                }
+            }
+        }
+        return -1;
+    }
+}
