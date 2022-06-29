dc4af4adad3b9af6fe768887a371721b10549c1f
angietest <gydms0136@gmail.com>
2022-06-29 20:37:37 +0900

    Update no9012.java

--- java/Backjoon/src/etc/no9012.java
+++ java/Backjoon/src/etc/no9012.java
@@ -6,34 +6,78 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class no9012 {
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		
-		for(int i = 0; i < T; i++) {
-			String S = br.readLine();
-			
-			//문자열의 각 자리의 값을 확인할때는 char형태로 바꿔서 확인하기 때문에 스택도 Character형으로 해준다.
-			Stack<Character> stack = new Stack<Character>();
-			
-			for(int j = 0; j < S.length(); j++) {
-				if(S.charAt(j) == '(') {
-					stack.push(S.charAt(j));
-				}else {
-					//비어있는지를 먼저 본다.
-					if(stack.empty()) {
-						stack.push(S.charAt(j));
-						break;
-					}else {
-						stack.pop();
-					}
-				}
-			}
-			if(stack.empty()) System.out.println("YES");
-			else System.out.println("NO");
-		}
-	}
+//	public static void main(String[] args) throws IOException {
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		int T = Integer.parseInt(br.readLine());
+//		
+//		for(int i = 0; i < T; i++) {
+//			String S = br.readLine();
+//			
+//			//문자열의 각 자리의 값을 확인할때는 char형태로 바꿔서 확인하기 때문에 스택도 Character형으로 해준다.
+//			Stack<Character> stack = new Stack<Character>();
+//			
+//			for(int j = 0; j < S.length(); j++) {
+//				if(S.charAt(j) == '(') {
+//					stack.push(S.charAt(j));
+//				}else {
+//					//비어있는지를 먼저 본다.
+//					if(stack.empty()) {
+//						stack.push(S.charAt(j));
+//						break;
+//					}else {
+//						stack.pop();
+//					}
+//				}
+//			}
+//			if(stack.empty()) System.out.println("YES");
+//			else System.out.println("NO");
+//		}
+//	}
+
+	//경현님 방식
+    private static String solution(String current) {
+
+        String ans = "YES";
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < current.length(); i++) {
+            Character temp = current.charAt(i);
+            if (temp == '(') {
+                stack.add(temp);
+
+            } else {
+                if (stack.isEmpty()) {
+                    ans = "NO";
+                    return ans;
+                } else {
+                    stack.pop();
+                }
+
+
+            }
+        }
+
+        if (stack.size() != 0) {
+            ans = "NO";
+        }
+
+
+        return ans;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            String current = br.readLine();
+            System.out.println(solution(current));
+
+        }
+
+    }
+
 
 }
 
