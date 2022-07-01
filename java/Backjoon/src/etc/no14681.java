08f8d6b25b7d9e0a6db82ff15556834a2099df43
angietest <gydms0136@gmail.com>
2022-07-01 23:50:39 +0900

    Create no14681.java

---
+++ java/Backjoon/src/etc/no14681.java
@@ -0,0 +1,19 @@
+package etc;
+
+import java.util.Scanner;
+
+public class no14681 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		// 4의 배수이면서, 100의 배수가 아닐 때 또는 400의 배수일 때
+		if ( (a % 4 == 0) && (a % 100 != 0 || a % 400 == 0) ) {
+			System.out.println("1");
+		}
+		else {
+			System.out.println("0");
+		}
+				
+		
+	}
+}
