3bc038a98712852963b6835111438daf3a2dc1fe
angietest <gydms0136@gmail.com>
2022-07-15 17:59:05 +0900

    Update ApplicationConfig.java

--- java/guestbook/src/main/java/kr/or/connect/guestbook/config/ApplicationConfig.java
+++ java/guestbook/src/main/java/kr/or/connect/guestbook/config/ApplicationConfig.java
@@ -7,6 +7,6 @@ import org.springframework.context.annotation.Import;
 @Configuration
 @ComponentScan(basePackages = { "kr.or.connect.guestbook.dao",  "kr.or.connect.guestbook.service"})
 @Import({ DBConfig.class })
-public class ApplicationCofig {
+public class ApplicationConfig {
 
 }
