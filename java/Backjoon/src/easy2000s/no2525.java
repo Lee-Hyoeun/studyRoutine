a2d282e133f1028425629c78ba4d6112d0bea0bd
Lee-Hyoeun <gydms0136@gmail.com>
2022-07-28 22:37:34 +0900

    Create no2525.java

---
+++ java/Backjoon/src/easy2000s/no2525.java
@@ -0,0 +1,30 @@
+package easy2000s;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+ 
+public class no2525 {
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+ 
+        int C = Integer.parseInt(br.readLine());
+ 
+        int min = 60 * A + B;   // 시 -> 분
+        min += C;
+ 
+        int hour = (min / 60) % 24;
+        int minute = min % 60;
+ 
+        System.out.println(hour + " " + minute);
+ 
+    }
+}
+
+
