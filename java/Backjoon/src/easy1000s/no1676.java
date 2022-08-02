5807d79aeb43d46a71aea7de23e8f321bb3bd186
Lee-Hyoeun <gydms0136@gmail.com>
2022-08-02 20:08:16 +0900

    Create no1676.java

---
+++ java/Backjoon/src/easy1000s/no1676.java
@@ -0,0 +1,23 @@
+package easy1000s;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
+public class no1676 {
+ 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		int num = Integer.parseInt(br.readLine());
+		int count = 0;
+ 
+		while (num >= 5) {
+			count += num / 5;
+			num /= 5;
+		}
+		System.out.println(count);
+	}
+ 
+}
+
