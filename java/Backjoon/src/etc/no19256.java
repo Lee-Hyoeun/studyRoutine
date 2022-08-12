9c38b399829050ca134393cbeb9497edb31a0a5f
Lee-Hyoeun <gydms0136@gmail.com>
2022-08-12 23:29:53 +0900

    Create no19256.java

---
+++ java/Backjoon/src/etc/no19256.java
@@ -0,0 +1,16 @@
+package etc;
+
+import java.util.Scanner;
+ 
+public class no19256 {
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		String lastString = "??!";
+		
+		String s = in.next();
+		System.out.println(s + lastString);
+	}
+}
+
