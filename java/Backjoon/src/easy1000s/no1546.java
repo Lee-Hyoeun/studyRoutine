473c34548333a5dcbd2f72f57013da50dd044aa8
Lee-Hyoeun <gydms0136@gmail.com>
2022-08-19 22:57:18 +0900

    Create no1546.java

---
+++ java/Backjoon/src/easy1000s/no1546.java
@@ -0,0 +1,27 @@
+package easy1000s;
+
+import java.util.Arrays;
+import java.util.Scanner;
+ 
+public class no1546 {
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+ 
+		double arr[] = new double[in.nextInt()];
+		
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = in.nextDouble();
+		}
+		in.close();
+		
+		double sum = 0;
+		Arrays.sort(arr);
+		
+		for(int i = 0; i < arr.length; i++) {
+			sum += ((arr[i] / arr[arr.length-1]) * 100);
+		}
+		System.out.print(sum / arr.length);
+	}
+}
+
