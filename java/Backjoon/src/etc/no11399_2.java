599494aa49c713bdd9c5ff442d05b18f3eee99ea
angietest <gydms0136@gmail.com>
2022-08-21 22:06:57 +0900

    change class name

--- java/Backjoon/src/etc/no11399_2.java
+++ java/Backjoon/src/etc/no11399_2.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
  
-public class Main {
+public class no11399_2 {
  
 	public static void main(String[] args) throws IOException {
  
--- java/Backjoon/src/etc/no9375.java
+++ java/Backjoon/src/etc/no9375.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 import java.util.HashMap;
 import java.util.StringTokenizer;
  
-public class Main {
+public class no9375 {
 	public static void main(String[] args) {
  
 		Scanner in = new Scanner(System.in);
