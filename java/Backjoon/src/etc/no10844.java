f7d540964a6d155154462fc8346f595e41a4b4c4
Lee-Hyoeun <gydms0136@gmail.com>
2022-09-04 21:51:14 +0900

    Update no10844.java

--- java/Backjoon/src/etc/no10844.java
+++ java/Backjoon/src/etc/no10844.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
  
-public class Main {
+public class no10844 {
 	
 	static Long[][] dp;
 	static int N;
