ef421c9f9fbfb16d9122782ac4871f868d1168e0
Lee-Hyoeun <gydms0136@gmail.com>
2022-09-07 23:52:02 +0900

    Update no2156_2.java

--- java/Backjoon/src/easy2000s/no2156_2.java
+++ java/Backjoon/src/easy2000s/no2156_2.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
  
-public class Main {
+public class no2156_2 {
 	
 	static Integer[] dp;
 	static int[] arr;
