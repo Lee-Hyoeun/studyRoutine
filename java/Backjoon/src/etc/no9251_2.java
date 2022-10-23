45c9ff6651bedba560318a853b02856330f90ad1
angietest <gydms0136@gmail.com>
2022-10-23 22:28:37 +0900

    Update no9251_2.java

--- java/Backjoon/src/etc/no9251_2.java
+++ java/Backjoon/src/etc/no9251_2.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
-public class no1951_2 {
+public class no9251_2 {
 
 	static char[] str1;
 	static char[] str2;
