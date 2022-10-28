ccca1a9420b7df1e2aef0f49cb1461b5b804243d
Lee-Hyoeun <gydms0136@gmail.com>
2022-10-28 23:55:16 +0900

    Create no15596.java

---
+++ java/Backjoon/src/etc/no15596.java
@@ -0,0 +1,13 @@
+package etc;
+
+class Test {
+    long sum(int[] a) {
+		long sum = 0;	// a 배열 정수 합 변수
+        
+		for(int i = 0; i < a.length; i++) {
+			sum += a[i];
+		}
+		return sum;
+	}
+}
+
