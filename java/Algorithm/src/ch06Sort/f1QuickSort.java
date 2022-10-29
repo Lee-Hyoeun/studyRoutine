3605c80d5ab495ce0ccbb6e26eac5e983eb4ff19
angietest <gydms0136@gmail.com>
2022-10-29 22:50:19 +0900

    Create f1QuickSort.java

---
+++ java/Algorithm/src/ch06Sort/f1QuickSort.java
@@ -0,0 +1,18 @@
+package ch06Sort;
+
+
+// 퀵 정렬
+public class f1QuickSort {
+	// 배열 요소 a[idx1]과 a[idx2]의 값을 바꿈
+	static void swap(int[] a, int idx1, int idx2) {
+		int t = a[idx1]; a[idx1] = a[idx2]; a[idx2] = t;
+	}
+	
+	// 퀵 정렬
+	static void quickSort(int[] a, int left, int right) {
+		
+		
+		
+	}
+
+}
