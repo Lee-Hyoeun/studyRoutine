6a3aa41c5772d7127e61d2818d1fbef3fd94f9fc
angietest <gydms0136@gmail.com>
2022-11-13 23:02:26 +0900

    Update Main.java

--- java/AATestCode/src/Main.java
+++ java/AATestCode/src/Main.java
@@ -3,13 +3,65 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 
-		//28번 정수 1개 입력받아 그대로 출력하기(단, 입력되는 정수의 범위 0~4,294,967,295)
-		Scanner sc = new Scanner(System.in);
+		//42번 정수 2개 입력받아 나눈 몫으로 출력하기
+	    Scanner sc = new Scanner(System.in);
+
+	    int a = sc.nextInt();
+	    int b = sc.nextInt();
+
+	    System.out.printf("%d",a/b);
 		
-		long num = sc.nextLong();
-		
-		System.out.println(num);
-		
-		  
 	}
 }
+
+
+////41번 문자 1개 입력받아 다음 문자 출력하기
+//Scanner sc = new Scanner(System.in);
+//
+//char ch = sc.next().charAt(0);
+//char nextCh = (char)((int)ch+1);
+//
+//System.out.printf("%c",nextCh);
+
+////40번 정수 1개 입력받아 부호바꿔 출력하기
+//Scanner sc = new Scanner(System.in);
+//
+//long a = sc.nextLong();
+//
+//System.out.printf("-%d",a);
+
+//		//38,39번 정수 2개 입력받아 합 출력하기
+//		Scanner sc = new Scanner(System.in);
+//		
+//		long a = sc.nextLong();
+//		long b = sc.nextLong();
+//		long c = a+b;
+//		System.out.println(c);
+
+
+////31번 10진정수 1개 입력받아 8진수로 출력하기
+//Scanner sc = new Scanner(System.in);
+//int num = sc.nextInt();
+//
+//System.out.printf("%o",num);
+
+////30번 정수 1개 입력받아 그대로 출력하기(단, 입력되는 정수 범위 -9223372036854775808 ~ +9223372036854775807)
+//Scanner sc = new Scanner(System.in);
+//  long lNum = sc.nextLong();
+//
+//  System.out.printf("%d", lNum);
+//
+
+////29번 실수 1개 입력받아 그대로 출력하기
+//// (단, 실수범위 +-1.7*10-308~+-1.7*10308, 소숫점 이하 11자리까지 반올림하여 출력)
+//Scanner sc = new Scanner(System.in);
+//double num = sc.nextDouble();
+//
+//System.out.printf("%.11f",num);
+
+////28번 정수 1개 입력받아 그대로 출력하기(단, 입력되는 정수의 범위 0~4,294,967,295)
+//Scanner sc = new Scanner(System.in);
+//
+//long num = sc.nextLong();
+//
+//System.out.println(num);
