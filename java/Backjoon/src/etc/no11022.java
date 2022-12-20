533530fa343c5e88bb3468315f79f9b46716bf1f
Lee-Hyoeun <gydms0136@gmail.com>
2022-12-20 22:25:43 +0900

    Create no11022.java

---
+++ java/Backjoon/src/etc/no11022.java
@@ -0,0 +1,21 @@
+package etc;
+
+import java.util.Scanner;
+ 
+public class no11022 {
+	public static void main(String args[]) {
+ 
+		Scanner in = new Scanner(System.in);
+ 
+		int T = in.nextInt();
+ 
+		for (int i = 1; i <= T; i++) {
+			int A = in.nextInt();
+			int B = in.nextInt();
+ 
+			System.out.println("Case #" + i + ": " + A + " + " + B + " = " + (A+B));
+		}
+ 
+		in.close();
+	}
+}
