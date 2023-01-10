4e9d56cf9f207cf10101d253e844d8fd3811cd69
Lee-Hyoeun <gydms0136@gmail.com>
2023-01-10 23:29:23 +0900

    Create no1916_2.java

---
+++ java/Backjoon/src/easy1000s/no1916_2.java
@@ -0,0 +1,66 @@
+package easy1000s;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class no1916_2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+
+		int[][] arr = new int[N+1][N+1];
+		for(int i = 1; i < N+1; i++) {
+			Arrays.fill(arr[i], -1); // 가중치 0이 있을수도 있으니 -1로 초기화 
+		}
+
+		for(int i = 0; i < M; i++) {
+			int a = sc.nextInt();
+			int d = sc.nextInt();
+			int c = sc.nextInt();
+
+			// 시작 정점과 도착 정점이 같은데 비용이 다른 값이 들어올 수 있기 때문에 예외 처리 
+			if(arr[a][d] == -1) 
+				arr[a][d] = c;
+			else if(arr[a][d] > c) 
+				arr[a][d] = c;
+		}
+
+		int start = sc.nextInt();
+		int end = sc.nextInt();
+
+		boolean[] check = new boolean[N+1]; // 정점이 집합 S에 속하는지 아닌지를 판별할 배열 
+
+		int[] distance = new int[N+1]; // 최단 거리를 담을 배열 
+		Arrays.fill(distance, Integer.MAX_VALUE-1); // 무한대로 초기화 
+
+		distance[start] = 0;
+
+		for(int i = 0; i < N-1; i++) { // 시작점을 넣고 시작하기 때문에 N-1만큼만 반복 
+			int min = Integer.MAX_VALUE;
+			int index = -1;
+
+			// 집합 S에 속하지 않는 가장 최단 거리를 갖는 정점 선택함 
+			for(int j = 1; j < N+1; j++) { 
+				if(!check[j] && distance[j] < min) {
+					min = distance[j]; // 최단 거리 
+					index = j; // 최단 거리를 갖는 정점의 index 
+				}
+			}
+
+			check[index] = true;
+
+			// S에 속하지 않는다면 더 작은 값을 갖는 거리로 distance값 갱신 
+			for (int j = 1; j < N+1; j++) { 
+				if (!check[j] && arr[index][j] != -1 && distance[index] + arr[index][j] < distance[j]) { // 간선이 연결되지 않은 -1의 경우 역시 제외해야함 
+					distance[j] = distance[index] + arr[index][j];
+				}
+			}
+		}
+
+		System.out.println(distance[end]); // 도착점까지의 최단 거리 출력 
+	}
+
+}
