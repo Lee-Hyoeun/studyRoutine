1e3bac7d6143cc034ad6f827333907e13b3e87d4
Lee-Hyoeun <gydms0136@gmail.com>
2023-03-12 23:52:34 +0900

    Create no10030.java

---
+++ java/Backjoon/src/etc/no10030.java
@@ -0,0 +1,30 @@
+package etc;
+
+import java.util.*;
+
+public class no10030 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int score[] = new int[5];
+		int sum = 0;
+		int avg;
+		for (int i = 0; i < score.length; i++) {
+			score[i] = sc.nextInt();
+			// 5의배수
+			if (score[i] % 5 != 0) {
+				i--;
+				System.out.println("5의배수를 입력해주세요");
+			}
+			// 40점 미만학생들은 40점으로 고정
+			if (score[i] < 40) {
+				score[i] = 40;
+			}
+			sum += score[i];
+		}
+		// 평균 = 총점 / 학생수
+		avg = sum / score.length;
+		System.out.println(avg);
+				
+	}
+}
+
