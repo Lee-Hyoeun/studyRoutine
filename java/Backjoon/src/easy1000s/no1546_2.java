9ef50c02d5cb29b3315eb85e5006f770590de2b6
Lee-Hyoeun <gydms0136@gmail.com>
2023-03-20 23:46:45 +0900

    Update and rename no1546.java to no1546_2.java

--- java/Backjoon/src/easy1000s/no1546_2.java
+++ java/Backjoon/src/easy1000s/no1546_2.java
@@ -2,7 +2,7 @@ package easy1000s;
 
 import java.util.*;
 
-public class no1546 {
+public class no1546_2{
 	public static void main(String[] args) {
 		/*
 		 	두 자연수 A와 B가 있을 때, A%B는 A를 B로 나눈 나머지 이다. 예를 들어, 7, 14, 27, 38을 3으로 나눈 나머지는 1, 2, 0, 2이다. 
