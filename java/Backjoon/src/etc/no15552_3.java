b1f835080d7a939ea7f4c8fa9cc764324f58cc0a
Lee-Hyoeun <gydms0136@gmail.com>
2023-03-27 23:57:42 +0900

    Create no15552_3.java

---
+++ java/Backjoon/src/etc/no15552_3.java
@@ -0,0 +1,27 @@
+package etc;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class no15552_3 {
+
+	public static void main(String[] args)throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine().trim());
+        
+        for (int i=0; i < n; i++) {
+            String text = br.readLine();
+            String[] word = text.split(" ");
+            int a = Integer.parseInt(word[0]);
+            int b = Integer.parseInt(word[1]);
+            bw.write((a+b) + "\n");
+        }
+        
+        bw.flush();
+        bw.close();
+	}
+
+}
