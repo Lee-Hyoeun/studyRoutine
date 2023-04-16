0d8ac58a548378aeedd3226fd388581f1022d70d
Lee-Hyoeun <gydms0136@gmail.com>
2023-04-16 23:46:33 +0900

    Create no10926.java

---
+++ java/Backjoon/src/etc/no10926.java
@@ -0,0 +1,14 @@
+package etc;
+
+import java.util.Scanner;
+
+public class no10926{
+    public static void main(String[] args) {
+
+        Scanner sc  = new Scanner(System.in);
+
+        String id = sc.nextLine();
+
+        System.out.println(id + "??!");
+    }
+}
