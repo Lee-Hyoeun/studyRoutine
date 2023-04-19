a399ac3603226d459450cf09048df64bf6799290
Lee-Hyoeun <gydms0136@gmail.com>
2023-04-19 23:57:42 +0900

    Create no12576_2.java

---
+++ java/Backjoon/src/etc/no12576_2.java
@@ -0,0 +1,23 @@
+package etc;
+
+import java.util.HashMap;
+
+class no12576_2 {
+    public boolean solution(String[] phone_book) {
+        
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < phone_book.length; i++) {
+            map.put(phone_book[i], i);
+        }
+        
+        for(int i =0; i< map.size(); i++){
+            for(int j=0; j<phone_book[i].length(); j++){
+                if(map.containsKey(phone_book[i].substring(0,j))) return false;
+            }
+        }
+        
+        return true;
+        
+    }
+}
