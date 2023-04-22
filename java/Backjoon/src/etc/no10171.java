e73f4a53fcc87020b5820bb1e5b1fa66fdcade43
Lee-Hyoeun <gydms0136@gmail.com>
2023-04-22 23:57:22 +0900

    Create no10171.java

---
+++ java/Backjoon/src/etc/no10171.java
@@ -0,0 +1,11 @@
+package etc;
+
+public class no10172 {
+    public static void main(String[] args) {
+
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
