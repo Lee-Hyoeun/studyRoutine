985d5dfe0658e69f5fac6564ed836bfcf11d2f32
Lee-Hyoeun <gydms0136@gmail.com>
2023-04-23 23:52:52 +0900

    Create no25083_2.java

---
+++ java/Backjoon/src/etc/no25083_2.java
@@ -0,0 +1,16 @@
+package etc;
+
+import java.util.Scanner;
+
+public class no25083_2{
+    public static void main(String[] args) {
+
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+        
+    }
+}
