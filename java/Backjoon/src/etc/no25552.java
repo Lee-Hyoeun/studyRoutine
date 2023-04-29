ed07ba0a6f073b24a80ff0fdc379ce98eb477e60
Lee-Hyoeun <gydms0136@gmail.com>
2023-04-29 22:53:09 +0900

    Create no25552.java

---
+++ java/Backjoon/src/etc/no25552.java
@@ -0,0 +1,27 @@
+package etc;
+
+import java.io.*;
+
+public class no25552 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int cnt = Integer.parseInt(br.readLine());
+
+        for(int i =0; i < cnt; i++){
+            String str = br.readLine();
+            int num1 = Integer.parseInt(str.split(" ")[0]);
+            int num2 = Integer.parseInt(str.split(" ")[1]);
+            bw.write(num1 + num2 + "\n");
+        }
+        
+        br.close();
+
+        bw.flush();
+        bw.close();
+
+    }
+}
+
