f30699ecd743f65ba946ad4f549061c0800c1cc0
Lee-Hyoeun <gydms0136@gmail.com>
2023-06-12 23:41:32 +0900

    Create no14425.java

---
+++ java/Backjoon/src/etc/no14425.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class no14425 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        HashMap<String, Integer> map = new HashMap<>();
+        int cnt = 0;
+
+        for (int i = 0; i < N; i++) {
+            map.put(br.readLine(), 1);
+        }
+        for (int i = 0; i < M; i++) {
+            String str = br.readLine();
+            if (map.get(str) != null) {
+                cnt++;
+            }
+        }
+        bw.write(cnt + "\n");
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
