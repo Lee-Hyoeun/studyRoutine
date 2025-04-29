f72dfa56fca2b589a9cd8768f410a1d0d60ff6b2
Lee-Hyoeun <gydms0136@gmail.com>
2025-04-29 23:59:43 +0900

    Create no1040.java

---
+++ java/Backjoon/src/easy1000s/no1040.java
@@ -0,0 +1,73 @@
+package easy1000s;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//이석원은 조규현과 백승환에게 상대편 마린(류재명)의 위치를 계산하라는 명령을 내렸다. 조규현과 백승환은 각각 자신의 터렛 위치에서 현재 적까지의 거리를 계산했다.
+//
+//조규현의 좌표 (x1, y1)와 백승환의 좌표 (x2, y2)가 주어지고, 조규현이 계산한 류재명과의 거리 r1과 백승환이 계산한 류재명과의 거리 r2가 주어졌을 때, 류재명이 있을 수 있는 좌표의 수를 출력하는 프로그램을 작성하시오.
+
+public class no1002 {
+	public static void main(String[] args) throws IOException {
+		 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+ 
+		int T = Integer.parseInt(br.readLine());
+ 
+		while (T-- > 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+ 
+			int x1 = Integer.parseInt(st.nextToken());
+			int y1 = Integer.parseInt(st.nextToken());
+			int r1 = Integer.parseInt(st.nextToken());
+ 
+			int x2 = Integer.parseInt(st.nextToken());
+			int y2 = Integer.parseInt(st.nextToken());
+			int r2 = Integer.parseInt(st.nextToken());
+			
+			System.out.println(tangent_point(x1, y1, r1, x2, y2, r2));
+		}
+ 
+	}
+ 
+ 
+	// 접점 개수 구하는 함수
+	public static int tangent_point(int x1, int y1, int r1, int x2, int y2, int r2) {
+    
+		int distance_pow = (int)(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));	// 중점간 거리 distance의 제곱 
+ 
+ 
+		// case 1 : 중점이 같으면서 반지름도 같을 경우
+		if(x1 == x2 && y1 == y2 && r1 == r2) {
+			return -1;
+		}
+		
+		// case 2-1 : 두 원의 반지름 합보다 중점간 거리가 더 길 때 
+		else if(distance_pow > Math.pow(r1 + r2, 2)) {
+			return 0;
+		}
+ 
+		// case 2-2 : 원 안에 원이 있으나 내접하지 않을 때 
+		else if(distance_pow < Math.pow(r2 - r1, 2)) {
+			return 0;
+		}
+		
+		// case 3-1 : 내접할 때 
+		else if(distance_pow == Math.pow(r2 - r1, 2)) {
+			return 1;
+		}
+        
+		
+		// case 3-2 : 외접할 때 
+		else if(distance_pow == Math.pow(r1 + r2, 2)) {
+			return 1;
+		}
+		
+		else {
+			return 2;
+		}
+		
+	}
